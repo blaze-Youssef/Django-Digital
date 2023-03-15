d35d963a9f482206f12dc742620f1708a32c1d4b
blaze-Youssef <83608661+blaze-Youssef@users.noreply.github.com>
2023-03-15 14:21:32 +0100

    s

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -75,27 +75,6 @@ WSGI_APPLICATION = "hello_django.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if not DATABASE_URL:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    db_info = urlparse(DATABASE_URL)
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": "db",
-            "USER": db_info.username,
-            "PASSWORD": db_info.password,
-            "HOST": db_info.hostname,
-            "PORT": db_info.port,
-            "OPTIONS": {"sslmode": "require"},
-        }
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
